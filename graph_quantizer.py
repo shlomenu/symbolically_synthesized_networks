dbd0821b9ff8226462bbc72397f39676781655cf
Eli Whitehouse <eliw55@gmail.com>
2023-02-09 14:45:16 -0500

    revise codebook vector-graph embedding
    Implements changes outlined in a previous commit message: codebook
    vectors are now embedded into graphs by distributing segments throughout
    the nodes/edges of the graph, thus making the same information available
    about the codebook vector available to the same extent for any choice of
    graph (though reconstructing it may require more or less complex
    structural understanding).
    
    In addition, edge dropout was added at each layer of the network.

--- graph_quantizer.py
+++ graph_quantizer.py
@@ -1,5 +1,6 @@
 import os
 import json
+import math
 
 import dgl
 import pygraphviz as pgv
@@ -26,8 +27,7 @@ class GraphQuantizer(Quantizer):
         self.codebook_dim = codebook_dim
         self.output_dim = output_dim
         self.max_conn = max_conn
-        self.emb_matrix = self.sincos_embedding_2d(
-            10, self.max_conn, self.codebook_dim, device=device)
+        self.create_sincos_embedding_2d(10, device=device)
         self.edge_dropout = nn.Dropout(dropout_rate)
         self.convs = nn.ModuleList([
             dgl.nn.GINEConv(  # type: ignore
@@ -35,71 +35,91 @@ class GraphQuantizer(Quantizer):
         ])
         self.node_dropouts = nn.ModuleList(
             [nn.Dropout(dropout_rate) for _ in range(depth)])
+        self.edge_dropouts = nn.ModuleList(
+            [nn.Dropout(dropout_rate) for _ in range(depth)]
+        )
         self.pool = dgl.nn.AvgPooling()  # type:ignore
         self.linear_out = nn.Linear(self.codebook_dim, output_dim)
 
-    @staticmethod
-    def sincos_embedding_2d(n_elt, max_conn, dim, device, temperature=10000):
-        assert (dim % 4) == 0, 'feature dimension must be multiple of 4 for sincos emb'
-        h, w = n_elt, max_conn + 1
+    def create_sincos_embedding_2d(self, size, device, temperature=10000):
+        assert (self.codebook_dim %
+                4) == 0, 'feature dimension must be multiple of 4 for sincos emb'
+        h, w = size, self.max_conn + 1
         x, y = th.meshgrid(th.arange(h), th.arange(w), indexing="xy")
-        omega = th.arange(dim // 4) / (dim // 4 - 1)
+        omega = th.arange(self.codebook_dim // 4) / \
+            (self.codebook_dim // 4 - 1)
         omega = 1. / (temperature ** omega)
         y = rearrange(y.flatten()[:, None] * omega[None, :],
-                      "(h w) qc -> h w qc", h=h, w=w, qc=(dim // 4))
+                      "(h w) qc -> h w qc", h=h, w=w, qc=(self.codebook_dim // 4))
         x = rearrange(x.flatten()[:, None] * omega[None, :],
-                      "(h w) qc -> h w qc", h=h, w=w, qc=(dim // 4))
-        return th.cat(
+                      "(h w) qc -> h w qc", h=h, w=w, qc=(self.codebook_dim // 4))
+        self.emb_matrix = th.cat(
             (x.sin(), x.cos(), y.sin(), y.cos()), dim=2).to(device=device, dtype=th.float)
 
-    @staticmethod
-    def graph_of_json(graph_json, emb_matrix, device):
-        edges, edges_1, edges_2, nodes, efeats, n_elt = set(), [], [], set(), [], -1
-        for [[node_1, port], [edge, node_2]] in graph_json["edges"]:
-            edge_canonical = (node_1, edge, node_2) if node_1 < node_2 else (
-                node_2, node_1)
-            if edge_canonical not in edges:
-                edges_1.append(node_1)
-                edges_2.append(node_2)
-                efeats.append([edge, port + 1])
-                nodes.add(node_1)
-                nodes.add(node_2)
-                n_elt = max((node_1, edge, node_2, n_elt))
-        edges_1, edges_2 = edges_1 + edges_2, edges_2 + edges_1
-        creation_ordered = sorted(nodes)
-        unit_spaced_to_creation_ordered = dict(
-            zip(range(len(nodes)), creation_ordered))
-        nfeats = [[unit_spaced_to_creation_ordered[n], 0]
-                  for n in range(len(nodes))]
-        creation_ordered_to_unit_spaced = dict(
-            zip(creation_ordered, range(len(nodes))))
-        data = (
-            th.tensor([creation_ordered_to_unit_spaced[node]
-                       for node in edges_1]),
-            th.tensor([creation_ordered_to_unit_spaced[node]
-                       for node in edges_2]))
-        if emb_matrix is None or n_elt + 1 > emb_matrix.size(0):
-            emb_matrix = GraphQuantizer.sincos_embedding_2d(n_elt + 1, emb_matrix.size(
-                1) - 1, emb_matrix.size(2), device=emb_matrix.device)
-        graph = dgl.to_homogeneous(dgl.graph(data, device=device))
-        nfeats_ind = th.empty(len(nfeats), 1, emb_matrix.size(
-            2), device=emb_matrix.device, dtype=th.long)
-        for i, [creation_order_id, port] in enumerate(nfeats):
-            nfeats_ind[i, port, :] = creation_order_id
-        efeats_ind_order = th.empty(len(efeats), emb_matrix.size(1), emb_matrix.size(
-            2), device=emb_matrix.device, dtype=th.long)
-        efeats_ind_port = th.empty(len(efeats), 1, emb_matrix.size(
-            2), device=emb_matrix.device, dtype=th.long)
-        for i, [creation_order_id, port] in enumerate(efeats):
-            for p in range(emb_matrix.size(1)):
-                efeats_ind_order[i, p, :] = creation_order_id
-            efeats_ind_port[i, 0, :] = port
-        nfeats = th.gather(emb_matrix, 0, nfeats_ind).squeeze()
-        efeats = th.gather(
-            th.gather(emb_matrix, 0, efeats_ind_order), 1, efeats_ind_port).squeeze()
-        if len(efeats.shape) == 1:
-            efeats = efeats.unsqueeze(0)
-        return (emb_matrix, graph, nfeats, efeats)
+    def graph_of_json(self, graph_json, choice_emb):
+        device = self.emb_matrix.device
+        seen_edges, edge_start, edge_end, node_order_ids, edge_order_ids, ports = \
+            set(), [], [], set(), [], []
+        for [[node_1_order_id, port], [edge_order_id, node_2_order_id]] in graph_json["edges"]:
+            edge_canonical = (node_1_order_id, edge_order_id, node_2_order_id) \
+                if node_1_order_id < node_2_order_id else (
+                    node_2_order_id, node_1_order_id)
+            if edge_canonical not in seen_edges:
+                edge_start.append(node_1_order_id)
+                edge_end.append(node_2_order_id)
+                edge_order_ids.append(edge_order_id)
+                ports.append(port + 1)
+                node_order_ids.add(node_1_order_id)
+                node_order_ids.add(node_2_order_id)
+        node_order_ids = sorted(node_order_ids)
+        n_nodes, n_edges = len(node_order_ids), len(edge_order_ids)
+        n_elt = n_nodes + n_edges
+        if n_elt > self.emb_matrix.size(0):
+            self.create_sincos_embedding_2d(n_elt, device=device)
+        nfeats_posemb_index = th.empty(
+            n_nodes, 1, self.codebook_dim, device=device, dtype=th.long)
+        efeats_posemb_order_index = th.empty(
+            n_edges, self.max_conn + 1, self.codebook_dim, device=device, dtype=th.long)
+        efeats_posemb_port_index = th.empty(
+            n_edges, 1, self.codebook_dim, device=device, dtype=th.long)
+        for i, order_id in enumerate(node_order_ids):
+            nfeats_posemb_index[i, 0, :] = order_id
+        for i, (order_id, port) in enumerate(zip(edge_order_ids, ports)):
+            for p in range(self.max_conn + 1):
+                efeats_posemb_order_index[i, p, :] = order_id
+            efeats_posemb_port_index[i, 0, :] = port
+        assert choice_emb.size(0) == self.codebook_dim
+        increment = self.codebook_dim // n_elt
+        remainder = self.codebook_dim - increment * n_elt
+        increments = list(range(increment + remainder,
+                          self.codebook_dim + 1, increment))
+        choice_matrix = th.cat(
+            [choice_emb[start:stop].tile(
+                (math.ceil(self.codebook_dim / increment),))[:self.codebook_dim].unsqueeze(0)
+                for start, stop in zip([0] + increments[:-1], increments)],
+            dim=0)
+        nfeats_choice_index = th.empty(
+            n_nodes, self.codebook_dim, device=device, dtype=th.long)
+        efeats_choice_index = th.empty(
+            n_edges, self.codebook_dim, device=device, dtype=th.long)
+        for i, order_id in enumerate(node_order_ids):
+            nfeats_choice_index[i, :] = order_id
+        for i, order_id in enumerate(edge_order_ids):
+            efeats_choice_index[i, :] = order_id
+        unit_spaced = dict(
+            zip(node_order_ids, range(len(node_order_ids))))
+        return (
+            dgl.to_homogeneous(dgl.graph((
+                th.tensor([unit_spaced[order_id]
+                          for order_id in edge_start + edge_end]),
+                th.tensor([unit_spaced[order_id]
+                          for order_id in edge_end + edge_start])), device=device)),
+            th.gather(
+                self.emb_matrix, 0, nfeats_posemb_index).squeeze(dim=1) +
+            th.gather(choice_matrix, 0, nfeats_choice_index),
+            (th.gather(
+                th.gather(self.emb_matrix, 0, efeats_posemb_order_index), 1, efeats_posemb_port_index).squeeze(dim=1) +
+             th.gather(choice_matrix, 0, efeats_choice_index)).tile((2, 1)))
 
     @staticmethod
     def pygraphviz_graph_of_json(graph_json, caption):
@@ -122,7 +142,7 @@ class GraphQuantizer(Quantizer):
         return graph
 
     def structural_prediction(self, latent, selections):
-        selections, device = selections.flatten().tolist(), selections.device
+        selections = selections.flatten().tolist()
         restarts = self.restart_manager.find_restarts(
             selections) if self.training else []
         graphs, nfeats, efeats, filenames = [], [], [], []
@@ -130,19 +150,15 @@ class GraphQuantizer(Quantizer):
             filename = self.representations[selection]
             with open(os.path.join(self.representations_path, filename)) as f:
                 repr = json.load(f)
-            self.emb_matrix, graph, nfeat, efeat = self.graph_of_json(
-                repr["output"], self.emb_matrix, device)
+            graph, nfeat, efeat = self.graph_of_json(repr["output"], latent[i])
             graphs.append(graph)
-            nfeats.append(nfeat + latent[i])
-            efeats.append((efeat + latent[i]).tile((2, 1)))
+            nfeats.append(nfeat)
+            efeats.append(efeat)
             filenames.append(filename)
-        graphs, nfeats, efeats = (
-            dgl.batch(graphs),
-            th.cat(nfeats, dim=0),
-            self.edge_dropout(th.cat(efeats, dim=0)),
-        )
-        for node_dropout, conv in zip(self.node_dropouts, self.convs):
-            nfeats = conv(graphs, node_dropout(nfeats), efeats)
+        graphs, nfeats, efeats = dgl.batch(graphs), th.cat(
+            nfeats, dim=0), th.cat(efeats, dim=0),
+        for node_dropout, edge_dropout, conv in zip(self.node_dropouts, self.edge_dropouts, self.convs):
+            nfeats = conv(graphs, node_dropout(nfeats), edge_dropout(efeats))
         return self.linear_out(self.pool(graphs, nfeats)), restarts, filenames
 
     def visualize(self, to_visualize=None):
